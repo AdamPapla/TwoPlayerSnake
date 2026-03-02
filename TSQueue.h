ab1265f7da758067405173b2b079493d55ba1579
Adam Graham <adampaplagraham000@gmail.com>
2026-03-02 22:05:24 +0000

    Add a thread-safe queue for future communication
    This change adds a thread-safe queue that supports one thread writing
    and another reading. This is added in anticipation of the required
    communication between the network layer and the application layer -
    messages must be produced by game state and consumed by the network
    layer and vice versa.
    
    Since game logic and network handling will run on separate threads, a
    thread safe queue will provide a cleaner separation and better performance
    than wrapping the actual game state in a mutex.

---
+++ TSQueue.h
@@ -0,0 +1,38 @@
+#include <queue>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+#include <optional>
+
+// A simple thread safe queue for passing data between threads
+template< typename T >
+class TSQueue {
+ public:
+   T pop() {
+      std::unique_lock< std::mutex > lk( mutex_ );
+      cv_.wait( lk, [ this ]{ return !queue_.empty(); });
+      T val = std::move( queue_.front() );
+      queue_.pop();
+      return std::move( val );
+   }
+   void push( T newElem ) {
+      {
+         std::lock_guard< std::mutex > lk( mutex_ );
+         queue_.push( std::move( newElem ) );
+      }
+      cv_.notify_one();
+   }
+   std::optional< T > try_front() const {
+      std::lock_guard< std::mutex > lk( mutex_ );
+      return queue_.empty() ? std::nullopt : queue_.front();
+   }
+   std::size_t size() const {
+      std::lock_guard< std::mutex > lk( mutex_ );
+      return queue_.size();
+   }
+   
+ private:
+   mutable std::mutex mutex_;
+   std::condition_variable cv_;
+   std::queue< T > queue_;
+};
