14d789b1650abb6990e714e98f84ab2373e0179d
Adam Graham <adampaplagraham000@gmail.com>
2026-03-07 18:02:24 +0000

    Add message definitions for server-client comm
    This change adds the initial message struct definitions for messages
    that will be streamed across the wire. These are still subject to
    change, but outline a rough idea of what we expect the message schema to
    look like.

---
+++ Messages.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <cassert>
+#include <cstdint>
+#include <cstring>
+#include <optional>
+#include <vector>
+
+#include "Utility.h"
+
+namespace Message {
+
+enum class ClientMessageType : std::uint16_t { REGISTER, JOIN, CHANGE_DIR, LEAVE };
+
+struct ClientMessage {
+   ClientMessageType msgType;
+};
+
+struct RegisterMessage : ClientMessage {
+   std::string name;
+   std::optional< ClientId > existingClientId_;
+};
+
+struct JoinMessage : ClientMessage {
+   std::string roomId;
+};
+
+struct ChangeDirMessage : ClientMessage {
+   Move newDir;
+};
+
+struct LeaveMessage : ClientMessage {};
+
+enum class ServerMessageType : std::uint16_t {
+   // Ack/Nack client messages
+   REGISTER_ACK,
+   REGISTER_NACK,
+   JOIN_ACK,
+   JOIN_NACK,
+   CHANGE_DIR_ACK,
+   CHANGE_DIR_NACK,
+   LEAVE_ACK,
+   LEAVE_NACK,
+
+   // Server response messages
+   DISCONNECT,
+   DEATH,
+   SNAPSHOT
+};
+
+struct ServerMessage {
+   ServerMessageType msgType;
+};
+
+struct RegisterAckMessage : ServerMessage {
+   ClientId id;
+};
+
+struct DisconnectMessage : ServerMessage {
+   std::string reason;
+};
+
+struct DeathMessage : ServerMessage {
+   uint32_t score;
+};
+
+struct Snapshot : ServerMessage {
+   std::vector< std::uint8_t > bytes;
+};
+
+}
